fb6a01f61118f961b9ae455d3f2983d70159750e
ZoltanSari <kingshapy@gmail.com>
2018-02-21 09:58:48 +0100

    Introduce second

--- gittie.py
+++ gittie.py
@@ -1,10 +1,8 @@
 def introduce():
-    print("Hello, I'm Gittie")
-<<<<<<< HEAD
+    print("Hello, I'm Agent_Cooper")
 
 def joke():
     print("Hahahaha")
-=======
+
 def add(a,b):
     return a+b    
->>>>>>> 4d23ec04d8b5426cc6385cbcc9b5e2c0921fb50b
